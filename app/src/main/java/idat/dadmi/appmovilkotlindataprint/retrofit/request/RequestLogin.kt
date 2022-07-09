80ecea7462a519219fc4db42abb2d38af4a62ec8
Lenin <leninalbino@gmail.com>
2022-07-09 01:51:02 -0500

    inicio

---
+++ app/src/main/java/idat/dadmi/appmovilkotlindataprint/retrofit/request/RequestLogin.kt
@@ -0,0 +1,3 @@
+package idat.dadmi.appmovilkotlindataprint.retrofit.request
+
+data class RequestLogin(var correo:String , var clave:String)
---
+++ app/src/main/java/idat/dadmi/appmovilkotlindataprint/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package idat.dadmi.appmovilkotlindataprint.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import idat.dadmi.appmovilkotlindataprint.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
