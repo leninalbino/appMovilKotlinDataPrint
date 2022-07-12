bb3f5396226a4e45cd5ed8e69649bf6e8b1db011
kirby239 <thumastercito428@gmail.com>
2022-07-11 23:19:34 -0500

    [kirby] subiendo avanze de kotlin

--- app/src/main/java/idat/dadmi/appmovilkotlindataprint/view/MainActivity.kt
+++ app/src/main/java/idat/dadmi/appmovilkotlindataprint/view/MainActivity.kt
@@ -1,7 +1,10 @@
 package idat.dadmi.appmovilkotlindataprint.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -38,18 +41,34 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navHomeFragment
+                R.id.HomeFragment,
+                R.id.CategoriaFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-
+    //direccionar login y carrito
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.login -> {
+                startActivity(Intent(this,LoginActivity::class.java))
+                return true;
+            }
+            R.id.carrito ->{
+                startActivity(Intent(this,CarritoActivity::class.java))
+                //Toast.makeText(this,"Carrito",Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    //fin direccionar login y carrito
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
