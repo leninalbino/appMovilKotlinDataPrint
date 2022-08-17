c7825113767e7aafc2e704befe6075cd93d54177
jhongarciamedina <jhonfarru.game@gmail.com>
2022-08-17 17:31:45 -0500

    [jhon]diseño

--- app/src/main/java/idat/dadmi/appmovilkotlindataprint/utilitarios/Constantes.kt
+++ app/src/main/java/idat/dadmi/appmovilkotlindataprint/utilitarios/Constantes.kt
@@ -4,7 +4,8 @@ class Constantes {
     // url del Back spring
     //val Data_Print_URL = "http://192.168.18.15:8086"
     //val Data_Print_URL = "http://192.168.1.4:8086"
-    val Data_Print_URL =  "http://192.168.18.4:8086"
+    //val Data_Print_URL =  "http://192.168.18.4:8086"
+    val Data_Print_URL =  "http://192.168.1.2:8086"
     val PREF_RECORDAR = "PREF_RECORDAR"
     val PREF_TOKEN = "PREF_TOKEN"
 }
